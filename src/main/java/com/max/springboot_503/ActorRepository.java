6c2e05cb11e42841d33a70a4d5d045dcf59fb08f
GBTC440004UR <GBTC440004UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-19 13:44:57 -0500

    first commit

---
+++ src/main/java/com/max/springboot_503/ActorRepository.java
@@ -0,0 +1,6 @@
+package com.max.springboot_503;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor, Long> {
+}
---
+++ src/test/java/com/max/springboot_503/Springboot503ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.springboot_503;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot503ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
